c9b37a90055170b93677c269d95656f6f77fa4fb
Iann-rst <iannrst11@gmail.com>
2022-11-02 18:29:39 -0300

    feat:  carregando fontes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, VStack, Text } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <VStack flex={1} bgColor="gray.300" alignItems="center" justifyContent="center">
-        <Text color="white" fontSize={24}>Hello World! Setup Mobile</Text>
-        <StatusBar style="auto" />
-      </VStack>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
