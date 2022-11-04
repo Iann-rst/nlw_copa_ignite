90907b14513a2e296b971a37732ec1c04abfc994
Iann-rst <iannrst11@gmail.com>
2022-11-04 15:49:08 -0300

    feat: Tela SignIn

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,22 @@
-import { Center, Text } from "native-base";
+import { Center, Text, Icon } from "native-base";
+import Logo from '../assets/logo.svg';
+import { Fontisto } from '@expo/vector-icons';
+
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24}>
-        SignIn
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+
+      <Button
+        title="Entrar com google"
+        leftIcon={<Icon as={Fontisto} name="google" size="md" color="white" />}
+        type="SECONDARY"
+        mt={12}
+      />
+      <Text color="gray.100" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
       </Text>
     </Center>
   )
