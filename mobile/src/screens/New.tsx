b0c6b489934ecf3a260a1908556822f722e2f514
Iann-rst <iannrst11@gmail.com>
2022-11-04 19:09:47 -0300

    feat: Tela Novo Bolão

---
+++ mobile/src/screens/New.tsx
@@ -0,0 +1,11 @@
+import { Text, VStack } from "native-base";
+
+export function New() {
+  return (
+    <VStack flex={1} bg="gray.900" alignItems="center" justifyContent="center">
+      <Text color="white">
+        Tela de novo bolão
+      </Text>
+    </VStack>
+  )
+}
