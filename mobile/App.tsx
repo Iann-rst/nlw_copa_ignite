e7300fe0fe08401118d4e6e3b72816771bec6483
Iann-rst <iannrst11@gmail.com>
2022-11-04 16:15:37 -0300

    feat: add contexto na aplicação

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,18 +5,21 @@ import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
 import { SignIn } from './src/screens/SignIn';
+import { AuthContextProvider } from "./src/context/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
