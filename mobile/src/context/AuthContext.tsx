ec15112cd11ee6a81ef2d5b75836c8d0f9bd5152
Iann-rst <iannrst11@gmail.com>
2022-11-04 18:55:52 -0300

    feat: Autenticação com o Google

--- mobile/src/context/AuthContext.tsx
+++ mobile/src/context/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -7,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
@@ -20,17 +26,43 @@ export const AuthContext = createContext({} as AuthContextDataProps);
 
 //Cria a função para prover o contexto
 export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '125450658191-6o7pso2i8raqol6m6u195va5a0k9210e.apps.googleusercontent.com',
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email']
+  });
+
 
   async function signIn() {
-    console.log("Vamos logar!")
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log("Token de autenticação => ", access_token);
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response])
 
   return (
     <AuthContext.Provider value={{
-      user: {
-        name: 'Iann',
-        avatarUrl: 'https://www.github.com/Iann-rst.png',
-      },
+      isUserLoading,
+      user,
       signIn
     }}>
       {children}
