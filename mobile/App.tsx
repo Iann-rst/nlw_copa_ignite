1b56c51fc49f77e3a937c6c9118b8716ec53a2a8
Iann-rst <iannrst11@gmail.com>
2022-11-05 17:31:32 -0300

    app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,6 +6,8 @@ import { Loading } from './src/components/Loading';
 import { THEME } from './src/styles/theme';
 import { SignIn } from './src/screens/SignIn';
 import { New } from './src/screens/New';
+import { Find } from './src/screens/Find';
+import { Pools } from './src/screens/Pools';
 import { AuthContextProvider } from "./src/context/AuthContext";
 
 export default function App() {
@@ -19,7 +21,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <New /> : <Loading />}
+        {fontsLoaded ? <Pools /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
