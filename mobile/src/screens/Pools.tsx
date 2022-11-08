2ebcb09756b824a3ad561268f2ea52a8e5707a27
Iann-rst <iannrst11@gmail.com>
2022-11-08 17:44:51 -0300

    feat: navegação para detalhes do bolão

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -60,7 +60,12 @@ export function Pools() {
           < FlatList
             data={polls}
             keyExtractor={item => item.id}
-            renderItem={({ item }) => <PoolCard data={item} />}
+            renderItem={({ item }) => (
+              <PoolCard
+                data={item}
+                onPress={() => navigation.navigate('details', { id: item.id })}
+              />
+            )}
             px={5}
             showsVerticalScrollIndicator={false}
             _contentContainerStyle={{ pb: 10 }}
