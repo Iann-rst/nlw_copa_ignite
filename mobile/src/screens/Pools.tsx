0d9b9ad508c0ec4d96108382c81ccc3f9a62b27a
Iann-rst <iannrst11@gmail.com>
2022-11-08 15:22:23 -0300

    feat: Listar os polls do usuário

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,13 +1,48 @@
-import { VStack, Icon } from "native-base";
-import { useNavigation } from '@react-navigation/native'
-
-import { Button } from "../components/Button";
-import { Header } from "../components/Header";
-
+import { useCallback, useEffect, useState } from "react";
+import { VStack, Icon, FlatList, useToast } from "native-base";
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 import { Octicons } from '@expo/vector-icons';
 
+import { api } from "../services/api";
+
+
+import { Loading } from '../components/Loading';
+import { Button } from "../components/Button";
+import { Header } from "../components/Header";
+import { PoolPros, PoolCard } from "../components/PoolCard"
+import { EmptyPoolList } from "../components/EmptyPoolList";
+
+
 export function Pools() {
+  const [polls, setPolls] = useState<PoolPros[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const toast = useToast();
   const navigation = useNavigation();
+
+  //Função para buscar os bolões que o usuário participa
+  async function fetchPolls() {
+    try {
+      setIsLoading(true)
+      const response = await api.get('/polls');
+      setPolls(response.data.polls);
+
+    } catch (error) {
+      console.log(error);
+      toast.show({
+        title: 'Não foi possível buscar os bolões',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchPolls();
+  }, []));
+
   return (
     <VStack flex={1} bg="gray.900">
       <Header title="Meus bolões" />
@@ -18,6 +53,21 @@ export function Pools() {
           onPress={() => navigation.navigate('find')}
         />
       </VStack>
+
+      {
+        isLoading ? <Loading /> :
+
+          < FlatList
+            data={polls}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => <PoolCard data={item} />}
+            px={5}
+            showsVerticalScrollIndicator={false}
+            _contentContainerStyle={{ pb: 10 }}
+            ListEmptyComponent={<EmptyPoolList />}
+          />
+
+      }
     </VStack>
   )
 }
