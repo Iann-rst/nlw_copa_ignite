c0765b3acd24d5283506d6ecc4f4d6811931e488
Iann-rst <iannrst11@gmail.com>
2022-11-08 18:54:00 -0300

    feat: adicionando variavel de ambiente

--- mobile/src/context/AuthContext.tsx
+++ mobile/src/context/AuthContext.tsx
@@ -31,7 +31,7 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
   const [isUserLoading, setIsUserLoading] = useState(false);
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    clientId: '125450658191-6o7pso2i8raqol6m6u195va5a0k9210e.apps.googleusercontent.com',
+    clientId: process.env.CLIENT_ID,
     redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
     scopes: ['profile', 'email']
   });
