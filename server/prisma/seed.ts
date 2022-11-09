d7ba355f2311b6f327fcf6fd68f0de860827bfb5
Iann-rst <iannrst11@gmail.com>
2022-11-09 19:37:34 -0300

    feat: populando tabela game do banco de dados

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -3,64 +3,46 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  /*Populando tabela user */
-  const user = await prisma.user.create({
-    data: {
-      name: 'John Doe',
-      email: 'johndoe@gmail.com',
-      avatarUrl: 'https://github.com/Iann-rst.png'
-    }
-  })
-
-  /*Populando tabela poll (bolões) */
-  const poll = await prisma.poll.create({
-    data: {
-      title: 'Example poll',
-      code: 'BOL123',
-      ownerId: user.id,
-
-      participants: {
-        create: {
-          userId: user.id
-        }
-      }
-    }
-  })
-
   /*Populando tabela game */
   await prisma.game.create({
     data: {
-      date: '2022-11-07T12:00:00.201Z',
-      firstTeamCountryCode: 'DE',
-      secondTeamCountryCode: 'BR'
+      date: '2022-11-24T16:00:00.201Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'RS'
     }
   })
 
-  /*Populando tabela game com 1 palpite*/
   await prisma.game.create({
     data: {
-      date: '2022-11-08T12:00:00.201Z',
-      firstTeamCountryCode: 'BR',
-      secondTeamCountryCode: 'AR',
-
-      guesses: {
-        create: {
-          firstTeamPoints: 2,
-          secondTeamPoints: 1,
-
-          participant: {
-            connect: {
-              userId_pollId: {
-                userId: user.id,
-                pollId: poll.id
-              }
-            }
-          }
-        }
-      }
+      date: '2022-11-24T07:00:00.201Z',
+      firstTeamCountryCode: 'CH',
+      secondTeamCountryCode: 'CM'
     }
   })
 
+  await prisma.game.create({
+    data: {
+      date: '2022-11-23T16:00:00.201Z',
+      firstTeamCountryCode: 'BE',
+      secondTeamCountryCode: 'CA'
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-23T13:00:00.201Z',
+      firstTeamCountryCode: 'ES',
+      secondTeamCountryCode: 'CR'
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-23T07:00:00.201Z',
+      firstTeamCountryCode: 'MA',
+      secondTeamCountryCode: 'HR'
+    }
+  })
 }
 
 
